23ec4d9a1eaa8d84d51776990d8ed95c0b6ab7d5
Chinthaka Ranasinghe <chinthaka.ranasinghe@go1.com>
2022-09-21 19:06:32 +1000

    dynamic test

---
+++ bodybuilder-dynamic-test.js
@@ -0,0 +1,132 @@
+var bodybuilder = require('bodybuilder');
+
+const req = {
+    "filter": {
+          "and": [
+            {
+                "eq": [
+                    {
+                        "fieldname" : "family_name",
+                        "value": "test"
+                
+                    }
+                ],
+            }
+            ,
+            {
+                "neq": [
+                    {
+                        "fieldname" : "given_name",
+                        "value": "sample"
+                
+                    }
+                ]
+            }
+        ]
+    },
+    "search": [{ 
+        "multi": [
+            { 
+                "option":"fields",
+                "option_value": ['family_name', 'given_name']
+            },
+            { 
+                "option":"query",
+                "option_value": "chinthaka"
+            },
+            { 
+                "option":"type",
+                "option_value": "phrase"
+            },
+            { 
+                "option":"lenient",
+                "option_value": true
+            }
+        ],
+        }],
+    "sort":[
+      {
+        "fieldname" : "family_name",
+        "order": "asc"
+      }
+   ],
+   "additional_details": [
+        {
+           "field_key" : "from",
+            "value": 0
+        },
+        {
+           "field_key" : "size",
+            "value": 200
+        }
+
+   ]
+};
+
+var body = bodybuilder();
+// processing the filters
+if (req.filter && req.filter.and.length > 0) {
+    req.filter.and.forEach((item) => {
+        let key = Object.keys(item);
+        switch (key[0]) {
+            case 'eq':
+                if (item.eq.length > 0) {
+                    item.eq.forEach((innerItem) => {
+                        body.query('term', innerItem.fieldname, innerItem.value);
+                    })
+                }
+                
+                break;
+        
+            case 'neq':
+                if (item.neq.length > 0) {
+                    item.neq.forEach((innerItem) => {
+                        body.notQuery('term', innerItem.fieldname, innerItem.value);
+                    })
+                }
+                break;
+        }
+    });
+}
+
+// if someone does a general search on the searchbox
+if (req.search) {
+    let obj = {};
+    req.search.forEach((val) => {
+        val.multi.forEach((innerVal) => {
+            
+            obj[innerVal.option] = innerVal.option_value;
+
+        });
+        body.filter('multi_match', obj);
+    });
+}
+
+// sorting 
+if (req.sort && req.sort.length > 0) {
+    let obj = [];
+    req.sort.forEach((val) => {
+        var o = {};
+        o[val.fieldname] = val.order;
+        obj.push(o);
+    });
+    body.sort(obj);
+}
+
+// additional columns
+if (req.additional_details && req.additional_details.length > 0) {
+    req.additional_details.forEach((val) => {
+        switch (val.field_key) {
+            case 'size':
+                body.size(val.value);
+                break;
+        
+            case 'from':
+                body.from(val.value);
+                break;
+        }
+    });
+}
+
+const res2 = body.build() // Build 2.x or greater DSL (default)
+console.log(JSON.stringify(res2));
